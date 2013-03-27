4b1acf31d6a4561bb538190fa49bd3e87ecab25e
Brett Huneycutt <brett@1000memories.com>
2013-03-26 21:32:36 -0700

    copy in assets

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  # send_file "public/day.html"
-  "Hello, world"
+  hour = Time.now.hour
+  day_or_night = (hour < 8 || hour > 20) ? "night" : "day"
+  send_file "public/index_#{day_or_night}.html"
 end
