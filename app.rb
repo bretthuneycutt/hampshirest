4d231b0c4cd232a1349124056c9264ddf8a6af4d
Brett Huneycutt <brett@1000memories.com>
2013-08-01 15:18:11 -0700

    fix timezone (heroku uses GMT)

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  hour = Time.now.hour
+  hour = Time.now.getlocal("-07:00").hour
   day_or_night = (hour < 8 || hour > 20) ? "night" : "day"
   send_file "public/index_#{day_or_night}.html"
 end
